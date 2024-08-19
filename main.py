16ebf2eee0665e0da494161baca0ed8866598da9
davidgilbertson <gilbertson.david@gmail.com>
2024-08-20 06:26:47 +1000

    Tweaks

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import math
 
 import streamlit as st
 from openai import OpenAI, Stream
+from openai.types.chat import ChatCompletionChunk
 
 st.set_page_config(page_title="GPT Confidence")
 
@@ -33,66 +34,75 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 
-def stream_to_html(stream: Stream):
+def stream_to_html(stream: Stream[ChatCompletionChunk]):
     html = ""
+    raw_text = ""
+
     with st.empty():
         for chunk in stream:
-            choice = chunk.choices[0]
             text = chunk.choices[0].delta.content
 
             if not text:
                 continue
 
-            text = escape(text)
-            content = choice.logprobs.content[0]
+            raw_text += text
+            content = chunk.choices[0].logprobs.content[0]
             if "\n" in content.token:
-                # Tokens can be :\n\n, etc, ideally these would be split into returns and content
                 html += content.token.replace("\n", "<br>")
             else:
                 prob = math.exp(content.logprob)
                 underline = f"3px solid rgba(255, 0, 0, {1-prob ** 1.6:.0%})"
                 options = [
-                    f"{escape(x.token)} ({math.exp(x.logprob):.2%})"
+                    f"{x.token} ({math.exp(x.logprob):.2%})"
                     for x in content.top_logprobs
                 ]
-                tooltip = "\n".join(options)
-                html += f"<span title='{tooltip}' style='border-bottom: {underline}'>{text}</span>"
+                tooltip = escape("\n".join(options))
+                text = escape(text)
+                html += f"<span title='\u200B{tooltip}' style='border-bottom: {underline}'>{text}</span>"
 
             st.html(html)
 
-    return html
+    return html, raw_text
 
 
 avatars = dict(assistant="open_ai_logo.svg", user=""":material/person:""")
 
 for message in st.session_state.messages:
     role = message["role"]
-    text = message["content"]
     with st.chat_message(role, avatar=avatars.get(role)):
         if role == "user":
-            st.markdown(text, unsafe_allow_html=True)
+            st.markdown(message["content"])
         else:
-            st.html(text)
+            st.html(message["html"])
 
 last_message = st.empty()
 
-if prompt := st.chat_input("E.g. Pick a number between 1 and 6"):
+if prompt := st.chat_input("E.g. Pick a number between 1 and 6", max_chars=500):
     st.session_state.messages.append(dict(role="user", content=prompt))
 
     with st.chat_message("user", avatar=avatars.get("user")):
         st.markdown(prompt)
 
     with st.chat_message("assistant", avatar=avatars.get("assistant")):
+        # Strip out the HTML before sending to API, and just the last few messages
+        messages: list[dict[str, str]] = [
+            dict(role=msg["role"], content=msg["content"])
+            for msg in st.session_state.messages[-8:]
+        ]
+
         stream = client.chat.completions.create(
             model="gpt-4o-mini",
-            messages=st.session_state.messages,
+            messages=messages,
             logprobs=True,
             top_logprobs=10,
+            max_tokens=1000,
             stream=True,
         )
-        text = stream_to_html(stream)
+        response_html, response_text = stream_to_html(stream)
 
-        st.session_state.messages.append(dict(role="assistant", content=text))
+        st.session_state.messages.append(
+            dict(role="assistant", content=response_text, html=response_html)
+        )
 
 if not st.session_state.messages:
     st.write("Type something in the input down the bottom 👇")
